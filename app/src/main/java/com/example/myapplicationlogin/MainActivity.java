56c08ca1ca96e5a319d7da30a1cc0fe9b2958762
Tshifhiwa Nevhuhulwi <vhuhulwit@gmail.com>
2023-01-09 11:37:21 -0800

    TextView to other pages linkage problems
    have to find a way to link my (register) and (forgot password) textviews to their relavent pages

--- app/src/main/java/com/example/myapplicationlogin/MainActivity.java
+++ app/src/main/java/com/example/myapplicationlogin/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.myapplicationlogin;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +14,8 @@ import android.widget.Toast;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Bundle savedInstanceState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +43,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+
+    public void click(View v) {
+        Intent intent;
+        switch(v.getId()) {
+            case R.id.register: // R.id.textView1
+                intent = new Intent(this, register.class);
+
+                            }
     }
 }
+
---
+++ app/src/main/java/com/example/myapplicationlogin/register.java
@@ -0,0 +1,4 @@
+package com.example.myapplicationlogin;
+
+public class register {
+}
